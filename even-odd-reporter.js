e5e91c5c81e67081c525a4d11caa4321ac56b67a
Jitske <80577121+happygeekme@users.noreply.github.com>
2021-04-23 10:57:55 +0200

    Add files via upload

---
+++ even-odd-reporter.js
@@ -0,0 +1,7 @@
+for (i = 0; i <= 20; i++) {
+    const remainder = (i % 2);
+    if (remainder === 0) {
+        console.log (i + " is even");
+    } 
+    console.log (i + " is oneven");
+};
---
+++ multiplication-tables.js
@@ -0,0 +1,6 @@
+for (i = 1; i <= 10; i++) {
+    for (tafelVan = 1; tafelVan <= 10; tafelVan++) {
+        console.log(i + " x " + tafelVan + " = " + (i * tafelVan));
+    }
+ 
+}
